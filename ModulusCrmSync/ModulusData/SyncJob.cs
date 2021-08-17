63ce514eeb90a3164652f6b449c51538be54b6ab
Jesper Nygaard Nielsen <jnn@laka.dk>
2021-08-17 15:44:17 +0200

    Opstart af projektet

---
+++ ModulusCrmSync/ModulusData/SyncJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModulusCrmSync.LAKAServices;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace ModulusCrmSync.ModulusData
+{
+    class SyncJob
+    {
+        public string EntityName { get; set; }
+        public string EntityLookupField { get; set; }
+        public string EntityUpdateField { get; set; }
+
+        public static List<SyncJob> HentSyncJobs()
+        {
+            List<SyncJob> SyncJobList = new List<SyncJob>();
+
+            LAKAServicesSoapClient client = new LAKAServicesSoapClient();
+            OracleConnection con = new OracleConnection(client.GetDataSource(ConfigurationManager.AppSettings["DataSource"].ToString()));
+            {
+                OracleCommand cmd = new OracleCommand("select * from CRMSYNC t ", con);
+                con.Open();
+                OracleDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    SyncJob sj = new SyncJob();
+                    sj.EntityName = dr["cprnr"].ToString();
+                    sj.EntityLookupField = dr["forbrugtetimer"].ToString();
+                    sj.EntityUpdateField = dr["resttimer"].ToString();
+                    SyncJobList.Add(sj);
+                }
+                return SyncJobList;
+            }
+        }
+    }
+}
--- ModulusCrmSync/Program.cs
+++ ModulusCrmSync/Program.cs
@@ -3,13 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System.Collections.Specialized;
+using System.ServiceModel.Description;
+using ModulusCrmSync.LAKAServices;
+using ModulusCrmSync.ModulusData;
 namespace ModulusCrmSync
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region Variable
+            LAKAServicesSoapClient client = new LAKAServicesSoapClient();
+            Uri urlcrm = new Uri(client.CRMOrgService("PROD"));
+            ClientCredentials credentials = new ClientCredentials();
+            credentials.Windows.ClientCredential.UserName = client.CRMOrgUsername();
+            credentials.Windows.ClientCredential.Password = client.CRMOrgPassword();
+            OrganizationServiceProxy serviceproxy = new OrganizationServiceProxy(urlcrm, null, credentials, null);
+            IOrganizationService service;
+            service = (IOrganizationService)serviceproxy;
+            #endregion
+
+            //string EntityName = "";
+            //string EntityLookupField = "";
+            //string EntityLookupFieldValue = "";
+            //string EntityUpdateField = "";
+            //string EntityUpdateFieldValue = "";
+
+
+
+            var CRMSyncJobs = SyncJob.HentSyncJobs();
+
+            foreach (SyncJob job in CRMSyncJobs)
+            {
+                QueryByAttribute q_readentity = new QueryByAttribute(job.EntityName)
+                {
+                    ColumnSet = new ColumnSet(new string[] { job.EntityLookupField })
+
+
+                };
+                q_readentity.Attributes.Add(job.EntityLookupField);
+                q_readentity.Values.Add(EntityLookupFieldValue);
+
+                Entity _entity = service.RetrieveMultiple(q_readentity).Entities.FirstOrDefault();
+                _entity.Attributes[job.EntityLookupField] = EntityUpdateFieldValue;
+
+                service.Update(_entity);
+            }
+
         }
     }
 }
